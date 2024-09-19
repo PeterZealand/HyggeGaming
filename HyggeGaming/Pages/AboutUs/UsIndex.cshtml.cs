5d4d8415105ce6cd669e6de6499da252bfcbbe69
Peter Patrick Eriksen <pete05q5@edu.zealand.dk>
2024-09-19 11:49:19 +0200

    Added About Us

---
+++ HyggeGaming/Pages/AboutUs/UsIndex.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HyggeGaming.Pages.AboutUs
+{
+    public class UsIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
