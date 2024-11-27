aaa507e6ef765bb336cc19937642e431c6879133
Peter Patrick Eriksen <pete05q5@edu.zealand.dk>
2024-11-27 12:51:29 +0100

    Created Create Team page

---
+++ HyggeGaming/Pages/Teams/CreateTeam.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HyggeGaming.Models;
+
+namespace HyggeGaming.Pages.Teams
+{
+    public class CreateTeamModel : PageModel
+    {
+        private readonly HyggeGaming.Models.HGDBContext _context;
+
+        public CreateTeamModel(HyggeGaming.Models.HGDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public DevTeam DevTeam { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.DevTeams.Add(DevTeam);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
