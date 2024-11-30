c0c203348adfd3948f18b204be5e15e6e7313330
Paprika Seidler <pas002@edu.zealand.dk>
2024-11-30 20:58:54 +0100

    CreateGame added. Minor changes on Profile (link added) and ManageGames page added

--- HyggeGaming/Services/Interfaces/IGameService.cs
+++ HyggeGaming/Services/Interfaces/IGameService.cs
@@ -5,6 +5,7 @@ namespace HyggeGaming.Services.Interfaces
     public interface IGameService
     {
         public IEnumerable<Game> GetGames();
-
+        public void AddGame(Game newGame);
+        public List<string> DevStages();
     }
 }
