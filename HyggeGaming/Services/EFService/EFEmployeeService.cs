b934af0213f9e722a2dc5df7f3a0f3402acb3eeb
Paprika Seidler <pas002@edu.zealand.dk>
2024-12-02 12:07:13 +0100

    some changes

--- HyggeGaming/Services/EFService/EFEmployeeService.cs
+++ HyggeGaming/Services/EFService/EFEmployeeService.cs
@@ -16,6 +16,10 @@ namespace HyggeGaming.Services.EFService
 
         public void AddEmployee(Employee Emp)
         {
+            context.Employees
+                .Include(e => e.Role)
+                .Include(e => e.ZipCodeNavigation)
+                .ToList();
             context.Employees.Add(Emp);
             context.SaveChanges();
             //return team;
