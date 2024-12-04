4517473099988c32f2e8b5ad6f3347a731e896b5
Peter Patrick Eriksen <pete05q5@edu.zealand.dk>
2024-12-04 20:52:05 +0100

    AllAssignments and Create Assignment + their services and methods coded
    + some work on Update Employee

---
+++ HyggeGaming/Pages/Assignments/CreateAssignment.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HyggeGaming.Models;
+using HyggeGaming.Services.Interfaces;
+
+namespace HyggeGaming.Pages.Assignments
+{
+    public class CreateAssignmentModel : PageModel
+    {
+        private readonly IAssignmentService AssignmentService;
+        private readonly IGameService GameService; // Assuming a service for fetching games.
+
+        [BindProperty]
+        public Assignment Assignment { get; set; }
+
+        public CreateAssignmentModel(IAssignmentService assignmentService, IGameService gameService)
+        {
+            AssignmentService = assignmentService;
+            GameService = gameService;
+        }
+
+        public IActionResult OnGet()
+        {
+            // Fetching games via the service and preparing for the dropdown.
+            ViewData["GameId"] = new SelectList(GameService.GetGames(), "GameId", "GameId");
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["GameId"] = new SelectList(GameService.GetGames(), "GameId", "GameId");
+                return Page();
+            }
+
+            // Add assignment via the service.
+            AssignmentService.CreateAssignment(Assignment);
+
+            return RedirectToPage("/Assignments/AllAssignments");
+        }
+    }
+}
